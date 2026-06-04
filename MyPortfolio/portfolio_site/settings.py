6df4631552e96af1f2684a0da495c0f6b928b140
Neson <nesonneson1@gmail.com>
2026-06-04 08:58:59 +0530

    fixed cloudinary deployment packages

--- MyPortfolio/portfolio_site/settings.py
+++ MyPortfolio/portfolio_site/settings.py
@@ -8,6 +8,9 @@ import os
 from pathlib import Path
 import dj_database_url
 from PIL import Image
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
