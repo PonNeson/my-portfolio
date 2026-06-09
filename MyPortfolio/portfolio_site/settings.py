a4bcf2fac196a3e8b923a17c9ac24db71ae9d4e6
Neson <nesonneson1@gmail.com>
2026-06-09 08:51:47 +0530

    Fix: Cloudinary resource type for PDF resume

--- MyPortfolio/portfolio_site/settings.py
+++ MyPortfolio/portfolio_site/settings.py
@@ -112,7 +112,8 @@ STATICFILES_DIRS = [
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dkoz3k7gp',
     'API_KEY': '568195923992155',
-    'API_SECRET': 'i5xCtubrqsGYwXdG0NBdcJMvRWU'
+    'API_SECRET': 'i5xCtubrqsGYwXdG0NBdcJMvRWU',
+    'RESOURCE_TYPE': 'auto',
 }
 
 # ஏதேனும் ஒரு ஸ்டேடிக் ஃபைல் விடுபட்டிருந்தாலும் பில்ட் ஃபெயில் ஆகாமல் தடுத்துவிடும்
