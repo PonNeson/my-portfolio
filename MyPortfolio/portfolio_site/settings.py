26a03b718d14a6aead603f2973dc73add269a46e
Neson <nesonneson1@gmail.com>
2026-06-14 18:07:52 +0530

    Fix: Added requirements.txt with pip freeze 2

--- MyPortfolio/portfolio_site/settings.py
+++ MyPortfolio/portfolio_site/settings.py
@@ -18,8 +18,7 @@ SECRET_KEY = 'django-insecure-1d89ru^ceqqhui3)#c)7+s*x@c^qp!4$4^t(%49w4#!kr&296&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-portfolio-wpr8.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['my-portfolio-1-ys1x.onrender.com', 'localhost', '127.0.0.1', '*']
 
 # Application definition
 
