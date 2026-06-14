9a638034443efd1493253a54e293814db5e3ad8c
Neson <nesonneson1@gmail.com>
2026-06-14 16:26:08 +0530

    fix;database 1

--- MyPortfolio/portfolio_site/settings.py
+++ MyPortfolio/portfolio_site/settings.py
@@ -69,12 +69,11 @@ WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 # Database Configuration
 DATABASES = {
     'default': dj_database_url.config(
-        # லிங்க் கடைசியில் ?sslmode=require சேர்த்திருக்கிறேன், பாருங்க:
-        default='postgresql://portfolio_db_gw3r_user:NM0b5lVhcxnh7zvo9cuXyqukHU8WDwtT@dpg-d809s2po3t8c73dir0q0-a.virginia-postgres.render.com/portfolio_db_gw3r?sslmode=require',
+        # require-க்கு பதிலா prefer என்று மாத்தியிருக்கிறேன்
+        default='postgresql://portfolio_db_gw3r_user:NM0b5lVhcxnh7zvo9cuXyqukHU8WDwtT@dpg-d809s2po3t8c73dir0q0-a.virginia-postgres.render.com/portfolio_db_gw3r?sslmode=prefer',
         conn_max_age=600
     )
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
